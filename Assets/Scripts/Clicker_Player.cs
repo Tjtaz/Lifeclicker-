b96702e4ca236f178a83f9a66a88d6c4fec4525d
MSI\Nico <lepilolodior7633@gmail.com>
2019-04-27 11:22:41 +0200

    oooooooooooooo

---
+++ Assets/Scripts/Clicker_Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker_Player : MonoBehaviour
+{
+    public float scorePlayer = 0f;
+    public float scoreToAdd = 10f;
+    public float nmbrEnemyKilled = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickEnemy()
+    {
+        scoreToAdd = scoreToAdd * nmbrEnemyKilled;
+        scorePlayer = scorePlayer += scoreToAdd;
+    }
+}
---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    public bool pauseOff;
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            togglePause();
+        }
+    }
+
+    bool togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            return (true);
+        }
+    }
+
+    // Main Menu
+
+    public void OnClickedButtonPlay()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnClickedButtonQuit()
+    {
+        Application.Quit();
+    }
+
+    // Pause Menu
+
+    public void OnClickedResume()
+    {
+        togglePause();
+    }
+
+    // Pause & End Menu
+
+    public void OnClickedRetry()
+    {
+        Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+    }
+
+    public void OnClickedMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
